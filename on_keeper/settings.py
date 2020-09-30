2310b162b6e9de39bf2aab920ae498de3492763f
Yerniyaz57 <bakhytov.yerniyaz@gmail.com>
2020-09-30 18:46:55 +0600

    5

--- on_keeper/settings.py
+++ on_keeper/settings.py
@@ -140,7 +140,7 @@ STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 
