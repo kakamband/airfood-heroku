026ceceeedaf091857dd88fbe07ff1fceefe0818
Yerniyaz57 <bakhytov.yerniyaz@gmail.com>
2020-09-30 18:33:02 +0600

    4

--- on_keeper/settings.py
+++ on_keeper/settings.py
@@ -147,7 +147,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CORS_ORIGIN_ALLOW_ALL = True
+
 CORS_ALLOW_CREDENTIALS = True
+
 
 
 ASGI_APPLICATION = "on_keeper.routing.application"
